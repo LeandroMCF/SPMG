da9f1d8a96a04d4549660b56126671c614ad28ea
Leandro Cavalcanti <73174539+LeandroMCF@users.noreply.github.com>
2021-06-01 12:33:32 -0300

    .

---
+++ SPMG-BackEnd/SP-Medical-Group/SP-Medical-Group/Controllers/ProntuarioController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SP_Medical_Group.Domains;
+using SP_Medical_Group.Interfaces;
+using SP_Medical_Group.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SP_Medical_Group.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ProntuarioController : ControllerBase
+    {
+        IProntuario _prontuario { get; set; }
+
+        public ProntuarioController()
+        {
+            _prontuario = new ProntuarioRepository();
+        }
+
+        
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(_prontuario.ListarProntuario());
+        }
+
+        [Authorize(Roles = "1, 2")]
+        [HttpPost]
+        public IActionResult BuscarCPF(Prontuario prontuario)
+        {
+            return Ok(_prontuario.BuscarPorCpf(prontuario.Cpf));
+        }
+
+        
+        [HttpPost("Cadastrar")]
+        public IActionResult Cadastrar(Prontuario novoProntuario)
+        {
+            _prontuario.CadastrarProntuario(novoProntuario);
+
+            return StatusCode(202);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            _prontuario.ExcluirProtuario(id);
+
+            return StatusCode(204);
+        }
+    }
+}
---
+++ SPMG-BackEnd/SP-Medical-Group/SP-Medical-Group/Repositories/ProntuarioRepository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SP_Medical_Group.Contexts;
+using SP_Medical_Group.Domains;
+using SP_Medical_Group.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SP_Medical_Group.Repositories
+{
+    public class ProntuarioRepository : IProntuario
+    {
+        SPMGContext ctx = new SPMGContext();
+
+        public Prontuario BuscarPorCpf(string cpf)
+        {
+            return ctx.Prontuarios
+                .Include(p => p.IdUsuarioNavigation)
+                .Select(p => new Prontuario
+                {
+                    IdProntuario = p.IdProntuario,
+                    Nome = p.Nome,
+                    DataDeNascimento = p.DataDeNascimento,
+                    Cpf = p.Cpf,
+                    Rg = p.Rg,
+                    Telefone = p.Telefone,
+                    Endereco = p.Endereco,
+
+                    IdUsuarioNavigation = new Usuario
+                    {
+                        Email = p.IdUsuarioNavigation.Email,
+                        IdTipoUsuario = p.IdUsuarioNavigation.IdTipoUsuario,
+
+                        IdTipoUsuarioNavigation = new TipoUsuario
+                        {
+                            TipoUsuario1 = p.IdUsuarioNavigation.IdTipoUsuarioNavigation.TipoUsuario1,
+                        }
+                    }
+                })
+                .FirstOrDefault(p => p.Cpf == cpf);
+        }
+
+        public void CadastrarProntuario(Prontuario novoProntuario)
+        {
+            ctx.Prontuarios.Add(novoProntuario);
+
+            ctx.SaveChanges();
+        }
+
+        public void ExcluirProtuario(int id)
+        {
+            Prontuario prontuario = ctx.Prontuarios.Find(id);
+
+            ctx.Prontuarios.Remove(prontuario);
+
+            ctx.SaveChanges();
+        }
+
+        public List<Prontuario> ListarProntuario()
+        {
+            return ctx.Prontuarios
+                .Include(p => p.IdUsuarioNavigation)
+                .Select(p => new Prontuario
+                {
+                    IdProntuario = p.IdProntuario,
+                    Nome = p.Nome,
+                    DataDeNascimento = p.DataDeNascimento,
+                    Cpf = p.Cpf,
+                    Rg = p.Rg,
+                })
+                .ToList();
+        }
+    }
+}
